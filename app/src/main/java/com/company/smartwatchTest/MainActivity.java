56e4f9e3ab66d45be929ba3cf8363065c60d37ed
alunarstorm <131702916+alunarstorm@users.noreply.github.com>
2023-07-30 08:23:30 +0200

    Update MainActivity.java

--- app/src/main/java/com/company/smartwatchTest/MainActivity.java
+++ app/src/main/java/com/company/smartwatchTest/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setPreference();
         setContentView(R.layout.activity_main);
+        
+    }
+    public String setPrefernece(){
+        return "english";
     }
 }
